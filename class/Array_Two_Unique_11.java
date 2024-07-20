a2d86108361393c84e228758cd82f337474145d6
piyushgoyall <109795757+piyushgoyall@users.noreply.github.com>
2024-07-20 23:11:16 +0530

    new

--- class/Array_Two_Unique_11.java
+++ class/Array_Two_Unique_11.java
@@ -1,33 +1,32 @@
-
 public class Array_Two_Unique_11 {
     public static void main(String[] args) {
         int[] arr = { 1, 5, 2, 3, 5, 1, 4, 3, 2, 6 };
         int res = 0;
-        // 1'st part
-        for(int i=0;i<arr.length;i++)
-        {
-            res = res ^ arr[i];
+
+        // 1st part: XOR all elements to get the XOR of the two unique numbers
+        for (int i = 0; i < arr.length; i++) {
+            res ^= arr[i];
         }
-        System.out.println(res);
-        // 2'nd part
-        int pos = 0; 
-        for(int i=0;i<32;i++)
-        {
-            if ((res & (i << i)) > 0) {
+
+        // 2nd part: Find the rightmost set bit in the XOR result
+        int pos = 0;
+        for (int i = 0; i < 32; i++) {
+            if ((res & (1 << i)) > 0) {
                 pos = i;
                 break;
             }
         }
-        // 3'rd part
+
+        // 3rd part: Divide the array elements into two groups and XOR separately
         int num1 = 0, num2 = 0;
-        for (int i = 0; i < arr.length; i++)
-        {
-            if((arr[i]&(1<<pos))>0)
-            {
-                num1 = num1 ^ arr[i];
+        for (int i = 0; i < arr.length; i++) {
+            if ((arr[i] & (1 << pos)) > 0) {
+                num1 ^= arr[i];
+            } else {
+                num2 ^= arr[i];
             }
-            
         }
-        System.out.println(pos);
+
+        System.out.println("The two unique numbers are: " + num1 + " and " + num2);
     }
 }
