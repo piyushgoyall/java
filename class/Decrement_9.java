95023bc2280e9c7d8cf115994aa378ccbcb04eb4
piyushgoyall <109795757+piyushgoyall@users.noreply.github.com>
2024-07-21 23:57:48 +0530

    new

--- class/Decrement_9.java
+++ class/Decrement_9.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
+
 public class Decrement_9 {
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int x = 1;
+        while ((n & x) == 0) {
+            n = n ^ x;
+            x = x << 1;
+        }
+        n = n ^ x;
+        System.out.println(n);
+
+        sc.close();
     }
 }
