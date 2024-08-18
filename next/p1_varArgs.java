7598c68bf2def8e02d95a09d7739e9f5eb24a1bd
piyushgoyall <109795757+piyushgoyall@users.noreply.github.com>
2024-08-18 23:57:39 +0530

    new

--- next/p1_varArgs.java
+++ next/p1_varArgs.java
@@ -1,6 +1,6 @@
 import java.util.Arrays;
 
-public class VarArgs {
+public class p1_varArgs {
     public static void main(String[] args) {
         func(); // Calling func with no arguments
         func(1, 2, 3); // Calling func with some arguments for demonstration
