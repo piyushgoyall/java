08f593b1ccc2fb4e0222d5da41dd22a180dd2ba7
piyushgoyall <109795757+piyushgoyall@users.noreply.github.com>
2024-08-20 23:36:56 +0530

    new

---
+++ next/p6_Jagged.java
@@ -0,0 +1,53 @@
+import java.util.Scanner;
+
+public class p6_Jagged {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+
+        int[][] arr = new int[n][m];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                arr[i][j] = sc.nextInt();
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        sc.close();
+    }
+}
+
+// public class p6_Jagged {
+// public static void main(String[] args) {
+// Scanner sc = new Scanner(System.in);
+// int n = sc.nextInt();
+
+// int[][] arr = new int[n][];
+// for (int i = 0; i < n; i++) {
+// int m = sc.nextInt();
+// arr[i] = new int[m];
+// for (int j = 0; j < m; j++) {
+// arr[i][j] = sc.nextInt();
+// }
+// }
+
+// System.out.println("\nArray: ");
+
+// for (int i = 0; i < n; i++) {
+// for (int j = 0; j < arr[i].length; j++) {
+// System.out.print(arr[i][j] + " ");
+// }
+// System.out.println();
+// }
+
+// sc.close();
+// }
+// }
---
+++ next/p7_rev.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class p7_rev {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        reverse(arr, n);
+
+        for (int i : arr) {
+            System.out.print(i + " ");
+        }
+
+        sc.close();
+    }
+
+    static void reverse(int[] arr, int n) {
+        int i = 0, j = n - 1;
+        while (i < j) {
+            swap(arr, i, j);
+            i++;
+            j--;
+        }
+    }
+
+    static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+}
